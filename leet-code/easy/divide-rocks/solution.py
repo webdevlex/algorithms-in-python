6887ebd0c1f95bd9d24099d06dc604f2d747354a
Alexis Martin <alexismartin1895@gmail.com>
2023-06-19 16:18:25 -0700

    completed divide rocks interveiew question

---
+++ leet-code/easy/divide-rocks/solution.py
@@ -0,0 +1,18 @@
+def solution(stones):
+    stones.sort()
+
+    pile1 = 0
+    pile2 = sum(stones)
+    i = 0
+    while pile1 < pile2 and i != (len(stones) - 1):
+        pile1 += stones[i]
+        pile2 -= stones[i]
+        i += 1
+
+    diffBeforeLastIter = abs((pile1 - stones[i - 1]) - (pile2 + stones[i - 1]))
+    currentDiff = abs(pile1 - pile2)
+
+    return min(currentDiff, diffBeforeLastIter)
+
+
+print(solution([15, 20, 3]))
