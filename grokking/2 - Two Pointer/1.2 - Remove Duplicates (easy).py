7ed3a6ed467e07c544cab677c7471d3b6e4b022e
Alexis Martin <alexismartin1895@gmail.com>
2023-07-30 15:31:51 -0700

    completed problems 1 and 2 in the two pointers section

---
+++ grokking/2 - Two Pointer/1.2 - Remove Duplicates (easy).py
@@ -0,0 +1,36 @@
+# Question: Given an array of sorted numbers, remove all duplicates from it. You should not use any extra space; after removing the duplicates in-place return the new length of the array.
+
+# Example 1:
+
+# Input: [2, 3, 3, 3, 6, 9, 9]
+# Output: 4
+# Explanation: The first four elements after removing the duplicates will be [2, 3, 6, 9].
+
+# Example 2:
+
+# Input: [2, 2, 2, 11]
+# Output: 2
+# Explanation: The first two elements after removing the duplicates will be [2, 11].
+
+
+def remove_duplicates(arr):
+    # index of the next non-duplicate element
+    next_non_duplicate = 0
+    i = 0
+
+    while i < len(arr):
+        if arr[i] != arr[next_non_duplicate]:
+            next_non_duplicate += 1
+            arr[next_non_duplicate] = arr[i]
+
+        i += 1
+
+    return next_non_duplicate + 1
+
+
+def main():
+    print(remove_duplicates([2, 3, 3, 3, 6, 9, 9]))
+    print(remove_duplicates([2, 2, 2, 11]))
+
+
+main()
