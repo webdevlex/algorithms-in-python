41d6befdd4db8736a3750a528998d75fe244190f
Alexis Martin <alexismartin1895@gmail.com>
2023-08-02 14:29:33 -0700

    updated question 1 of chapter 1

--- cracking-the-code-interview/ch 1 - Arrays and Strings/1.1 - Is Unique.py
+++ cracking-the-code-interview/ch 1 - Arrays and Strings/1.1 - Is Unique.py
@@ -1,17 +1,43 @@
-def isUnique(str):
-    if len(str) > 128:
+# Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
+# cannot use additional data structures?
+
+
+# Solution 1: With additional data structures
+def is_unique_chars1(s):
+    if len(s) > 128:
         return False
 
-    chars = [False] * 128
-    for char in str:
-        i = ord(char) - ord("a")
-        if chars[i]:
+    char_set = [False] * 128
+    for char in s:
+        val = ord(char)
+        if char_set[val]:
             return False
-        chars[i] = True
+        char_set[val] = True
 
     return True
 
 
+# Solution 2: With additional data structures
+def is_unique_chars2(s):
+    checker = 0
+    for char in s:
+        val = ord(char) - ord("a")
+        if checker & (1 << val) > 0:
+            return False
+        checker |= 1 << val
+    return True
+
+
+# Soution 3: If we are allowed to modify the input string, we could sort the string in
+# O(n log(n)) time and then linearly check the string for neighboring characters
+# that are identical. Be careful, though many sorting algorithms take up extra
+# space.
+
+
 input = "Test"
-output = isUnique(input)
-print(f'\nInput: "{input}"\nOutput: {output}')
+
+output1 = is_unique_chars1(input)
+print(f'\nInput: "{input}"\noutput: {output1}')
+
+output2 = is_unique_chars2(input)
+print(f'\nInput: "{input}"\noutput: {output2}')
