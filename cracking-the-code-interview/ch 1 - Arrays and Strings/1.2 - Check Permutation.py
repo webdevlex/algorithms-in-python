5db93e2337a5d69ad902cdc500a3006298e75a0e
Alexis Martin <alexismartin1895@gmail.com>
2023-08-02 15:27:19 -0700

    updated problem 2 of chapter 1

--- cracking-the-code-interview/ch 1 - Arrays and Strings/1.2 - Check Permutation.py
+++ cracking-the-code-interview/ch 1 - Arrays and Strings/1.2 - Check Permutation.py
@@ -1,4 +1,22 @@
-def checkPermutation(str1, str2):
+# Check Permutation: Given two strings, write a method to decide if one is a
+# permutation of the other.
+
+
+# Solution 1
+def sort_string(s):
+    content = list(s)
+    content.sort()
+    return "".join(content)
+
+
+def is_permutation1(s, t):
+    if len(s) != len(t):
+        return False
+    return sort_string(s) == sort_string(t)
+
+
+# Solution 2
+def is_permutation2(str1, str2):
     if len(str1) != len(str2):
         return False
 
@@ -18,6 +36,10 @@ def checkPermutation(str1, str2):
 
 input1 = "Test"
 input2 = "teTs"
-output = checkPermutation(input1, input2)
 print(f'\nInput: "{input1}", "{input2}"')
+
+output = is_permutation1(input1, input2)
+print(f"Output: {output}")
+
+output = is_permutation2(input1, input2)
 print(f"Output: {output}")
