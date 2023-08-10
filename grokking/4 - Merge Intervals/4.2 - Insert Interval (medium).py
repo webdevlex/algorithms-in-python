d417f1544322fb261e2cd7b7101d597705e8e8ef
Alexis Martin <alexismartin1895@gmail.com>
2023-08-09 18:11:07 -0700

    completed problem 3 of the merge intervals section

---
+++ grokking/4 - Merge Intervals/4.2 - Insert Interval (medium).py
@@ -0,0 +1,68 @@
+# Given a list of non-overlapping intervals sorted by their start time,
+# insert a given interval at the correct position and merge all necessary intervals
+# to produce a list that has only mutually exclusive intervals.
+
+# Example 1:
+
+# Input: Intervals=[[1,3], [5,7], [8,12]], New Interval=[4,6]
+# Output: [[1,3], [4,7], [8,12]]
+# Explanation: After insertion, since [4,6] overlaps with [5,7],
+# we merged them into one [4,7].
+
+# Example 2:
+
+# Input: Intervals=[[1,3], [5,7], [8,12]], New Interval=[4,10]
+# Output: [[1,3], [4,12]]
+# Explanation: After insertion, since [4,10] overlaps with [5,7] & [8,12],
+# we merged them into [4,12].
+
+# Example 3:
+
+# Input: Intervals=[[2,3],[5,7]], New Interval=[1,4]
+# Output: [[1,4], [5,7]]
+# Explanation: After insertion, since [1,4] overlaps with [2,3],
+# we merged them into one [1,4].
+
+
+def insert(intervals, new_interval):
+    merged = []
+    i, start, end = 0, 0, 1
+
+    # Skip (and add to output) all intervals that come before the ‘new_interval’
+    while i < len(intervals) and intervals[i][end] < new_interval[start]:
+        merged.append(intervals[i])
+        i += 1
+
+    # Merge all intervals that overlap with ‘new_interval’
+    while i < len(intervals) and intervals[i][start] <= new_interval[end]:
+        new_interval[start] = min(intervals[i][start], new_interval[start])
+        new_interval[end] = max(intervals[i][end], new_interval[end])
+        i += 1
+
+    # Insert the new_interval
+    merged.append(new_interval)
+
+    # Add all the remaining intervals to the output
+    while i < len(intervals):
+        merged.append(intervals[i])
+        i += 1
+
+    return merged
+
+
+def main():
+    print(
+        "Intervals after inserting the new interval: "
+        + str(insert([[1, 3], [5, 7]], [8, 12]))
+    )
+    print(
+        "Intervals after inserting the new interval: "
+        + str(insert([[1, 3], [5, 7]], [4, 18]))
+    )
+    print(
+        "Intervals after inserting the new interval: "
+        + str(insert([[2, 3], [5, 7]], [1, 4]))
+    )
+
+
+main()
---
+++ grokking/4 - Merge Intervals/4.3 - Intervals Intersection (medium).py
@@ -0,0 +1,65 @@
+# Problem Statement #
+
+# Given two lists of intervals, find the intersection of these two lists.
+# Each list consists of disjoint intervals sorted on their start time.
+
+# Example 1:
+
+# Input: arr1=[[1, 3], [5, 6], [7, 9]], arr2=[[2, 3], [5, 7]]
+# Output: [2, 3], [5, 6], [7, 7]
+# Explanation: The output list contains the common intervals between the two lists.
+
+# Example 2:
+
+# Input: arr1=[[1, 3], [5, 7], [9, 12]], arr2=[[5, 10]]
+# Output: [5, 7], [9, 10]
+# Explanation: The output list contains the common intervals between the two lists.
+
+
+def merge(intervals_a, intervals_b):
+    result = []
+    i, j, start, end = 0, 0, 0, 1
+
+    while i < len(intervals_a) and j < len(intervals_b):
+        # Check if intervals overlap and intervals_a[i]'s start time lies
+        a_overlaps_b = (
+            intervals_a[i][start] >= intervals_b[j][start]
+            and intervals_a[i][start] <= intervals_b[j][end]
+        )
+
+        # Check if intervals overlap and intervals_b[j]'s start time lies
+        b_overlaps_a = (
+            intervals_b[j][start] >= intervals_a[i][start]
+            and intervals_b[j][start] <= intervals_a[i][end]
+        )
+
+        # Store the intersection part
+        if a_overlaps_b or b_overlaps_a:
+            result.append(
+                [
+                    max(intervals_a[i][start], intervals_b[j][start]),
+                    min(intervals_a[i][end], intervals_b[j][end]),
+                ]
+            )
+
+        # Move to the next interval which is finishing first
+        if intervals_a[i][end] < intervals_b[j][end]:
+            i += 1
+        else:
+            j += 1
+
+    return result
+
+
+def main():
+    print(
+        "Intervals Intersection: "
+        + str(merge([[1, 3], [5, 6], [7, 9]], [[2, 3], [5, 7]]))
+    )
+    print(
+        "Intervals Intersection: "
+        + str(merge([[1, 3], [5, 7], [9, 12]], [[5, 10], [9, 11]]))
+    )
+
+
+main()
