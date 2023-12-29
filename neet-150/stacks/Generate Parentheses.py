29e6faaec6b6e8086289a504dd03cfb859600801
Alexis Martin <alexismartin1895@gmail.com>
2023-12-28 18:57:48 -0800

    solved Generate Parentheses

--- neet-150/stacks/Generate Parentheses.py
+++ neet-150/stacks/Generate Parentheses.py
@@ -3,4 +3,22 @@ from typing import List
 
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        pass
+        stack = []
+        res = []
+
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
+                return
+
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+
+        backtrack(0, 0)
+        return res
