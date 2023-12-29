8e9f0c29bece146044061c5adfef74b8d51b7385
Alexis Martin <alexismartin1895@gmail.com>
2023-12-28 18:41:49 -0800

    solved Daily Termperature

--- neet-150/arrays-&-hashing/Two Sum.py
+++ neet-150/arrays-&-hashing/Two Sum.py
@@ -1,39 +1,12 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        myDict = {}
-        for i, num in enumerate(nums):
-            if num in myDict:
-                myDict[num] = [myDict[num], i]
-            else:
-                myDict[num] = i
-
-            requiredNum = target - num
-            if requiredNum in myDict:
-                if isinstance(myDict[requiredNum], list):
-                    return myDict[requiredNum]
-                elif myDict[requiredNum] != i:
-                    return [myDict[requiredNum], i]
+from typing import List
 
 
-mySolution = Solution()
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}  # val -> index
 
-# Example 1
-input = [2, 7, 11, 15]
-target = 9
-output = mySolution.twoSum(input, target)
-print(f"\nInput: nums = {input}, target = {target}")
-print(f"Output: {output}\n")
-
-# Example 2
-input = [3, 2, 4]
-target = 6
-output = mySolution.twoSum(input, target)
-print(f"Input: nums = {input}, target = {target}")
-print(f"Output: {output}\n")
-
-# Example 3
-input = [3, 3]
-target = 6
-output = mySolution.twoSum(input, target)
-print(f"Input: nums = {input}, target = {target}")
-print(f"Output: {output}\n")
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
